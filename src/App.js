afe66e03c34319eb2dfb3e3e1e6f417c2c4f3aa0
Stephen Gontzes <gontzess@gmail.com>
2022-01-07 19:18:39 -0500

    Co-authored-by: idkraut <idkraut@users.noreply.github.com> Co-authored-by: Josh Keller <wboard82@users.noreply.github.com> Co-authored-by: Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { routes, airlines, airports } from './data'
 
@@ -21,16 +21,34 @@ const columns = [
   { name: 'Destination Airport', property: 'dest', format: getAirportByCode },
 ]
 
-const Table = ({ columns, rows }) => {
+const Table = ({ columns, rows, perPage }) => {
+  const [start, setStart] = useState(0)
+
+  const handlePrev = () => {
+    let newStart = start - perPage
+    if (newStart <= 0) {
+      newStart = 0
+    } 
+    setStart(newStart)
+  }
+
+  const handleNext = () => {
+    if (start + perPage >= rows.length) return
+    setStart(start + perPage)
+  }
+
   return (
+    <>
+    <button onClick={handlePrev} disabled={start <= 0}>Prev Page</button>
+    <button onClick={handleNext} disabled={start + perPage >= rows.length}>Next Page</button>
     <table>
-      <caption>Routes</caption>
+      <caption>Showing {start + 1}-{start + perPage} of {rows.length} routes</caption>
       <tr>
         {columns.map(column => (
           <th>{column.name}</th>
         ))}
       </tr>
-      {rows.map(row => (
+      {rows.slice(start, start + perPage).map(row => (
         <tr>
           {columns.map(column => (
             <td>{column.format(row[column.property])}</td>
@@ -38,17 +56,35 @@ const Table = ({ columns, rows }) => {
         </tr>
       ))}
     </table>
+    </>
   )
 }
 
 const App = () => {
+  const [selectedAirline, setSelectedAirline] = useState("all")
+
+  const filterRoutesByAirline = (routes, selectedAirline) => {
+    if (selectedAirline === "all") {
+      return routes;
+    }
+    return routes.filter(route => route.airline === Number(selectedAirline));
+  }
+
+  const displayedRoutes = filterRoutesByAirline(routes, selectedAirline);
+
   return (
     <div className="app">
       <header className="header">
         <h1 className="title">Airline Routes</h1>
       </header>
       <section>
-        <Table className="routes-table" columns={columns} rows={routes} />
+        <form>
+          <select onChange={(event) => setSelectedAirline(event.target.value)} value={selectedAirline}>
+            <option value="all">All Airlines</option>
+            {airlines.map(airline => <option value={airline.id}>{airline.name}</option>)}
+          </select>
+        </form>
+        <Table className="routes-table" columns={columns} rows={displayedRoutes} perPage={25}/>
       </section>
     </div>
   )
